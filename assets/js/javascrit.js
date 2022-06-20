ff37e57f52054fa2cdd26e532f9fc082007ec39b
makovka2011 <47387956+makovka2011@users.noreply.github.com>
2022-06-20 17:51:25 +0500

    Add files via upload

---
+++ assets/js/javascrit.js
@@ -0,0 +1,70 @@
+//alert('Привет, мир!')
+//console.log('Hello,world');
+//console.log(7+3);
+//let phone = '+79093333333';
+//console.log(phone);
+//const COLOR_RED="#F0F0F0";
+//console.log(COLOR_RED)
+//const colors=['#f0f0f0','#ffffff','black','red'];
+//console.log(colors)
+//let int = 5;
+//let float = 5.98;
+//let int = 010;
+//int=055;
+//int = 0xFF;
+//int = 0xB8;
+//let str1= '"ECMAScript"';
+/*function myFunc(str) {
+    console.log(str);
+}
+const result = myFunc('JavaScript');
+console.log(result);
+
+function sum(a,b) {
+    console.log(a+b);
+}
+sum(2,3);
+
+//document.getElementById("main").textContent = "Привет, Мир";
+const $btn = document.querySelector('#my-btn');
+$btn.onclick = function() {
+    alert('Вы нажали на кнопку');
+}
+function changeBgColor() {
+    document.body.style.backgroundColor = `rgb(${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)}, ${Math.round(Math.random()*255)})`;
+}
+document.onclick = changeBgColor;
+
+
+let text = 'Вкусный суп';
+let a = text.charAt(0);
+
+console.log(a);
+
+let a = ['Тише', 'мыши', 'кот на крыше'];
+let b = a.join(',');
+console.log(b);
+
+
+
+for(let i= 1; i<=3; i++){
+    console.log(i);
+}
+
+
+function CalcSum(a = 0, b = 0, c = 5){
+    var d = a + b + c;
+    if(d < 10){
+        return 0;
+    }else if(10 < d && d < 20){
+        return d + 50;
+    }else if(100 < d){
+        return 100;
+    }
+    return d;
+    
+}
+var x = 5, y = 5, z = 5
+let a = CalcSum(x,y,z);
+console.log(a);
+*/
